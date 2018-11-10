328a53b4469d1c964e359ce39124a79561fc9ed8
Darius_EC <dariusciuverca@gmail.com>
2018-11-10 21:29:31 +0200

    Initiate VS project.

---
+++ Arduino_testing/Arduino_testing/Arduino_testing.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino_testing.ino
+ Created:	11/10/2018 9:28:44 PM
+ Author:	Darius Emanuel
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
